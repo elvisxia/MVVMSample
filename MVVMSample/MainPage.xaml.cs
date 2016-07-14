83a7090a77732cbdd9601571069416bdb27b2661
v-xucxia <v-xucxia@PC-ELVISXIA>
2016-07-14 12:34:10 +0800

    Add project files.

---
+++ MVVMSample/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MVVMSample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MVVMSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        
+
+        private async void myBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+            var file=await picker.PickSingleFileAsync();
+            IRandomAccessStream randomStream=await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            WriteableBitmap bitmap = new WriteableBitmap(500, 500);
+            bitmap.SetSource(randomStream);
+            ViewModel.Original = bitmap;
+            
+        }
+        
+    }
+}
---
+++ MVVMSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MVVMSample.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        { }
+        public MainPageViewModel(WriteableBitmap original)
+        {
+            this.Original = original;
+        }
+
+
+        public WriteableBitmap _original;
+        public WriteableBitmap Original
+        {
+            get { return this._original; }
+            set {
+                this._original = value;
+                RaisePropertyChanged();
+                }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName]string propertyName="")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
